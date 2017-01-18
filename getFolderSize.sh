68f2e5b86129590bc5232517e903ef0cb672b104
kjsjk <mulekea3@gmail.com>
2017-01-18 12:50:38 +0300

    Update getFolderSize.sh

--- getFolderSize.sh
+++ getFolderSize.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+#Just a basic script
+echo "Intializing the script..."
+cd  /; # move into the desired directory
+fileList=$(ls -d */); #list all directories in the folder
 
+for f in $fileList; 
+	do 
+
+	d=$(du -hs $f); #get the file size of each folder
+	dd=${d//\/}; #remove trailing forward slashes
+	echo $dd; #>> /home/amuleke/foldersList.txt;
+
+	done;
