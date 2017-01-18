9c034cde9351382176fe80307bc316c5164fe973
kjsjk <mulekea3@gmail.com>
2017-01-18 12:55:27 +0300

    Update getFolderSize.sh

--- getFolderSize.sh
+++ getFolderSize.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #Just a basic script
 echo "Intializing the script..."
-cd  /; # move into the desired directory
-fileList=$(ls -d */); #list all directories in the folder
+cd  /; # move into the desired directory, change this to your target directory e.g ( /var/www/ )
+fileList=$(ls -d */); #list all directories in the folder (Ignores files, gets folders only)
 
-for f in $fileList; 
+for f in $fileList; #loop through the list of folders found
 	do 
-
-	d=$(du -hs $f); #get the file size of each folder
-	dd=${d//\/}; #remove trailing forward slashes
-	echo $dd; #>> /home/amuleke/foldersList.txt;
+	# execute recursive action on the each here
+	
+	# In this example we are getting folder size and writing to a text file
+	   d=$(du -hs $f); #get the size of each folder
+	   dd=${d//\/};    #remove trailing forward slashes from the text
+	   echo $dd; >> /targetFolder/targetFile.txt; #write to text file
 
 	done;
